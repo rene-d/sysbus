fc5eb42a2c72d32d4d967c10811bbb9d4f0bd871
rene-d <rene-d@users.noreply.github.com>
2020-01-25 23:51:49 +0100

    bump to v1.0.3

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ with open(path.join(here, "README.en.md"), encoding="utf-8") as f:
 
 setup(
     name="sysbus",
-    version="1.0.2",
+    version="1.0.3",
     description="Control by script your Livebox 2,3,4",
     long_description=long_description,
     long_description_content_type="text/markdown",
